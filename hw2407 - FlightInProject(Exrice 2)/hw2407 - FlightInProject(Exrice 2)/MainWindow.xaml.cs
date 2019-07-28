f95cad5b5f0be29dfcc7470b3bfb508ecace3663
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-07-28 19:16:28 +0300

    Add files via upload

---
+++ hw2407 - FlightInProject(Exrice 2)/hw2407 - FlightInProject(Exrice 2)/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using FlightManagementProject;
+using FlightManagementProject.Facade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hw2407___FlightInProject_Exrice_2_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModelForFlight mvvm = new ViewModelForFlight(); 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = mvvm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mvvm.MyFlight = new AnonymousUserFacade().GetFlightById(Convert.ToInt32(numberFlightTxtBx.Text));
+        }
+    }
+}
---
+++ hw2407 - FlightInProject(Exrice 2)/hw2407 - FlightInProject(Exrice 2)/ViewModelForFlight.cs
@@ -0,0 +1,39 @@
+﻿using FlightManagementProject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2407___FlightInProject_Exrice_2_
+{
+    class ViewModelForFlight : INotifyPropertyChanged
+    {
+        private Flight myFlight;
+        public Flight MyFlight
+        {
+            get
+            {
+                return this.myFlight;
+            }
+            set
+            {
+                myFlight = value;
+                OnPropertyChanged("MyFlight");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModelForFlight()
+        {
+            myFlight = new Flight();
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
